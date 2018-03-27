beee5166626a008ba85a6a1a2bb15aacfadbdc18
sivagopal <sivagopaltech@gmail.com>
2018-03-27 22:56:08 +0530

    string

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -7,14 +7,16 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	string value = "this is a string";
 	cout << "!!!Hello there World!!!" << endl; // prints !!!Hello World!!!
 	cout << "hi" << "hello" << "how are you" << endl;
 	cout << "this is a test" << endl;
 	int numberOfCats = 5;
 	cout << "number of cats " << numberOfCats << endl;
-
+	cout << value << endl;
 	return 0;
 }
